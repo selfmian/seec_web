1ede410ff288a9164fcd9ec53ed91c3542acc624
薛皓腾 <11252787+xuehaoteng@user.noreply.gitee.com>
2022-07-07 10:50:56 +0800

    第一次提交，初始化项目仓库

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    首页
+  </div>
+</template>
+
+<script>
+import instance from "@/api/api"
+export default {
+  name: "HomeView",
+  created() {
+    instance.get("").then(res => console.log(res))
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
