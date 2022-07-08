dc97428fe4c27a519f666c9b15a70c0102676c5b
薛皓腾 <11252787+xuehaoteng@user.noreply.gitee.com>
2022-07-08 10:42:21 +0800

    改回原来的标题

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,7 @@
       :rules="loginRules"
       class="login-form"
     >
-      <h3 class="title">东兴市智慧化管理平台 你好</h3>
+      <h3 class="title">东兴市智慧化管理平台</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
